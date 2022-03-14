7e62533ddd566d76fc1b6cee4e9b040aed0097ed
sla <slavanovosyolov@gmail.com>
2022-03-14 17:56:11 +0300

    Добавлено задание 1 и 2

--- TrainingPractice_01/NSD_Task_01/Program.cs
+++ TrainingPractice_01/NSD_Task_01/Program.cs
@@ -4,9 +4,43 @@ namespace NSD_Task_01
 {
     internal class Program
     {
+        public static int user_gold;
+        public static int crystals_count;
+        public static int crystal_cost = 15;
+        public static int crystals_player_can_buy = 0;
+
+        public delegate void Delegate();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("dsdsd");
+            Delegate del;
+            StartDialog();
+            
+            del = (crystals_count * crystal_cost) <= user_gold ? PurchuaseSuccess : PurchuaseFailed;
+
+            del();
+        }
+
+        private static void StartDialog()
+        {
+            Console.WriteLine("Добро пожаловать в Магазин \"У Гнома\". Для начала, скажите, сколько у Вас имеется золота?");
+            user_gold = int.Parse(Console.ReadLine());
+            Console.WriteLine("Отлично! Я могу предложить Вам наш лучший товар - кристаллы! " +
+                $"Стоймость каждого - всего {crystal_cost} золота." +
+                "\nИтак, сколько кристалов Вы бы хотели приобрести?");
+            crystals_count = int.Parse(Console.ReadLine());
+        }
+
+        private static void PurchuaseSuccess()
+        {
+            Console.WriteLine($"Поздравляем с приобретением {crystals_count} кристалов!" +
+                $"\nУ Вас осталось {user_gold - crystals_count * crystal_cost} золота.");
+        }
+
+        private static void PurchuaseFailed()
+        {
+            Console.WriteLine($"Сожалеем, но у Вас не хватает {crystals_count * crystal_cost - user_gold} золота на приобретение {crystals_count} кристалов!" +
+                $"\nВаше золото {user_gold}.");
         }
     }
 }
---
+++ TrainingPractice_01/NSD_Task_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NSD_Task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            while (true)
+            {
+                Console.Write("Введите exit, чтобы выйти: ");
+                text = Console.ReadLine();
+                if (text == "exit") break;
+            }
+        }
+    }
+}
