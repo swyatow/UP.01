b6782f93fdb9fa6120f1b58b0a8459eca7143255
sla <slavanovosyolov@gmail.com>
2022-03-14 18:18:32 +0300

    Добавлено задание 3

---
+++ TrainingPractice_01/NSD_Task_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NSD_Task_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret_message = "Поздравляем! Вы открыли Секретное сообщение!";
+            string password = "password";
+
+            int count_of_tries = 1;
+
+            while (count_of_tries <= 3)
+            {
+                Console.WriteLine("Введите параоль");
+                if(Console.ReadLine() == password)
+                {
+                    Console.WriteLine(secret_message);
+                    break;
+                }else
+                {
+                    Console.WriteLine($"Пароль не верный! Осталось {3 - count_of_tries} попыток.\nПосле трех попыток, программа будет закрыта1");
+                    count_of_tries++;
+                }
+            }
+        }
+    }
+}
