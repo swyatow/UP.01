be595ed9041697fa18163bd54b08e393d2bded01
sla <slavanovosyolov@gmail.com>
2022-03-14 13:43:07 +0300

    Create Repos

---
+++ TrainingPractice_01/NSD_Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSD_Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dsdsd");
+        }
+    }
+}
