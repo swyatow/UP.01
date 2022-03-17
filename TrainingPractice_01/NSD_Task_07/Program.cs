89cfa2c545c10af9d29b66f4d110f851f8b8e363
sla <slavanovosyolov@gmail.com>
2022-03-17 21:40:30 +0300

    Добавлено задание 7

---
+++ TrainingPractice_01/NSD_Task_07/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NSD_Task_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] oldArray = new int[10];
+            int[] newArray;
+
+            Console.WriteLine("Старый массив:");
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                oldArray[i] = random.Next();
+                Console.Write(oldArray[i] + " ");
+            }
+            Console.WriteLine("\nНовый массив:");
+            newArray = Shuffle(oldArray, random);
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write(newArray[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+        public static int[] Shuffle(int[] oldArray, Random rnd)
+        {
+            for (int i = oldArray.Length - 1; i >= 1; i--)
+            {
+                int j = rnd.Next(i + 1);
+                int temp = oldArray[j];
+                oldArray[j] = oldArray[i];
+                oldArray[i] = temp;
+            }
+
+            return oldArray;
+        }
+    }
+}
